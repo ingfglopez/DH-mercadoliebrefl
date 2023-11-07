330d830305efe66bddb9fd9d5bc3e326c285ac5f
Fernando Lopez <ingfglopez@gmail.com>
2023-11-06 21:21:20 -0300

    modificacion

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-cons PORT=process.env.PORT ?? 3000;
+const PORT=process.env.PORT ?? 3000;
 
 app.listen(PORT, ()=>{
     console.log('Servidor funcionando');
